5318ed886c6762ae4fe8a4aca76a976d3d74aa4a
Damian Suhaj <a1747931@student.adelaide.edu.au>
2021-05-24 19:55:47 +0930

    Voting page form added. Need to get semantic-ui working to properly display the page.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,9 @@
 class HomepageController < ApplicationController
   def index
   end
+
+  def voter
+    @candidates = Candidate.all
+    @parties = ['Xick Nenophon Party', 'Liberals', 'Labor', 'Greens']
+  end
 end

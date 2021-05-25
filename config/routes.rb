6a7b3733eae83f37599b7d4b112c4cfdba19618a
Damian Suhaj <a1747931@student.adelaide.edu.au>
2021-05-25 14:06:20 +0930

    Added admin views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "homepage#index"
   get '/voter', to: 'homepage#voter'
+  get '/admin', to: 'homepage#admin'
+  get '/admin/results', to: 'homepage#results'
+  get '/admin/candidates', to: 'candidates#index'
 
   resources :candidates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
